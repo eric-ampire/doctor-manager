5a449dacbecbfa229cd417f2968db821461adb97
Eric Ampire <ericampire.top@gmail.com>
2018-11-09 13:55:11 +0200

    Ajout du login activity et son layout

---
+++ app/src/main/java/com/pbreakers/medprox/authentification/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.pbreakers.medprox.authentification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pbreakers.medprox.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
