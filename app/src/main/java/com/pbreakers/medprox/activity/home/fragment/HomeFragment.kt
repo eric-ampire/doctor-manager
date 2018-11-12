c81fd7edfaf7c9fc9f199469e49c3920089ddd5a
Eric Ampire <ericampire.top@gmail.com>
2018-11-12 22:16:08 +0200

    Ajout du nouveau fragmentHome

--- app/src/main/java/com/pbreakers/medprox/activity/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/home/fragment/HomeFragment.kt
@@ -4,17 +4,27 @@ package com.pbreakers.medprox.activity.home.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 import com.pbreakers.medprox.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        return true
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        bottomNavigation.setOnNavigationItemSelectedListener(this)
     }
 }
