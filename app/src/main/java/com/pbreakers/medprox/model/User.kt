303475ae6b6a8825d2d2274e376aa8e49265d177
Eric Ampire <ericampire.top@gmail.com>
2018-11-13 11:35:37 +0200

    Ajout d'un nouveau model

--- app/src/main/java/com/pbreakers/medprox/model/User.kt
+++ app/src/main/java/com/pbreakers/medprox/model/User.kt
@@ -1,5 +1,7 @@
 package com.pbreakers.medprox.model
 
+import androidx.appcompat.widget.DialogTitle
+
 data class User(
     var id: String = "",
     var login: String = ""
@@ -9,4 +11,14 @@ data class TypePartenaire(
     var idType: String = "",
     var nomType: String = "",
     var descripType: String = ""
+)
+
+data class Notification(
+    var idNotification: String = "",
+    var dateDebut: Int = 0,
+    var dateFin: Int = 0,
+    var title: String = "",
+    var contenu: String = "",
+    var cible: String = "",
+    var urlPhoto: String = ""
 )
