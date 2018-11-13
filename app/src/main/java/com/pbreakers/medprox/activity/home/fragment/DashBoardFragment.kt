c7cb5b86fec0c016a85dc40db8d750ba8cac068f
Eric Ampire <ericampire.top@gmail.com>
2018-11-13 09:18:40 +0200

    Mise en place de l'adapter pour affiche les elements du dashboard

--- app/src/main/java/com/pbreakers/medprox/activity/home/fragment/DashBoardFragment.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/home/fragment/DashBoardFragment.kt
@@ -8,6 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.pbreakers.medprox.R
+import com.pbreakers.medprox.model.TypePartenaire
+import com.xwray.groupie.Group
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.fragment_dash_board.*
+import kotlinx.android.synthetic.main.item_dashboard.view.*
 
 
 class DashBoardFragment : Fragment() {
@@ -16,5 +23,26 @@ class DashBoardFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dash_board, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val adapter = GroupAdapter<ViewHolder>()
+        rvListItem.adapter = adapter
+
+        adapter.add(DashBoardItem(TypePartenaire(nomType = "Pharmacie", descripType = "Trouver pr")))
+        adapter.add(DashBoardItem(TypePartenaire(nomType = "Banque de sang", descripType = "Trouver pr")))
+        adapter.add(DashBoardItem(TypePartenaire(nomType = "Hopital", descripType = "Trouver pr")))
+        adapter.add(DashBoardItem(TypePartenaire(nomType = "Medecin", descripType = "Trouver un medecin")))
+    }
+
+    class DashBoardItem(val type: TypePartenaire) : Item<ViewHolder>() {
+        override fun getLayout(): Int = R.layout.item_dashboard
+
+        override fun bind(viewHolder: ViewHolder, position: Int) {
+            with(viewHolder.itemView) {
+                tvNomType.text = type.nomType
+                tvDescripType.text = type.descripType
+            }
+        }
     }
 }
