fc37b6788907ffa97dc8e196226193d474535f89
Eric Ampire <ericampire.top@gmail.com>
2018-11-13 20:51:08 +0200

    Ajout du fragment permettant a l'utilisateur de restaurer sont mot de passe

---
+++ app/src/main/java/com/pbreakers/medprox/activity/authentification/fragment/ResetCodeFragment.kt
@@ -0,0 +1,32 @@
+package com.pbreakers.medprox.activity.authentification.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.pbreakers.medprox.R
+
+class ResetCodeFragment : Fragment() {
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reset_code, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+    }
+}
