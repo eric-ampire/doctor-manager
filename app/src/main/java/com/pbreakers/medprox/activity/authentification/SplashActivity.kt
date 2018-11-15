8fdd1010e80d29b916beb2dc5471269084d9ac34
Eric Ampire <ericampire.top@gmail.com>
2018-11-15 10:58:21 +0200

    On verifie si l'utilisateur s'est deja authentifier

--- app/src/main/java/com/pbreakers/medprox/activity/authentification/SplashActivity.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/authentification/SplashActivity.kt
@@ -1,13 +1,32 @@
 package com.pbreakers.medprox.activity.authentification
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pbreakers.medprox.R
+import com.pbreakers.medprox.activity.home.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        object : Thread() {
+            override fun run() {
+                Thread.sleep(1000)
+
+                val userPreferences = getSharedPreferences("user-info", Context.MODE_PRIVATE)
+
+                if (userPreferences == null) {
+                    startActivity(Intent(baseContext, LoginActivity::class.java))
+                    finish()
+                } else {
+                    startActivity(Intent(baseContext, MainActivity::class.java))
+                    finish()
+                }
+            }
+        }.start()
     }
 }
