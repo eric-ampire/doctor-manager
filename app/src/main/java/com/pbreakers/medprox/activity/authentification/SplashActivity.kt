c30a2a702c46f3685e270a1c3beffd1633cc8d72
Eric Ampire <ericampire.top@gmail.com>
2018-11-15 10:49:42 +0200

    Ajout du splash

---
+++ app/src/main/java/com/pbreakers/medprox/activity/authentification/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.pbreakers.medprox.activity.authentification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pbreakers.medprox.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
