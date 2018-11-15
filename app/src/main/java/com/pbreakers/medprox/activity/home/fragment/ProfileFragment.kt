9b680fa176bf10f2aa5040a01626749933d09e36
Eric Ampire <ericampire.top@gmail.com>
2018-11-15 16:45:47 +0200

    Ajout des informations sur le fragment

--- app/src/main/java/com/pbreakers/medprox/activity/home/fragment/ProfileFragment.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/home/fragment/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.pbreakers.medprox.activity.home.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.pbreakers.medprox.R
+import com.pbreakers.medprox.model.User
+import kotlinx.android.synthetic.main.fragment_identity.*
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
@@ -24,6 +28,17 @@ class ProfileFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+        loadProfileData()
+    }
+
+    private fun loadProfileData() {
+        val userInfo = activity!!.getSharedPreferences(User.KEY_USER, Context.MODE_PRIVATE)
+
+        edNomProfile.setText(userInfo.getString(User.NOM_USER, ""))
+        edPostNomProfile.setText(userInfo.getString(User.POSTNOM_USER, ""))
+        edPrenomProfile.setText(userInfo.getString(User.PRENOM_USER, ""))
+        edGenreProfile.setText(userInfo.getString(User.GENRE_USER, ""))
+        edAdresseProfile.setText(userInfo.getString(User.ADRESSE_USER, ""))
     }
 }
