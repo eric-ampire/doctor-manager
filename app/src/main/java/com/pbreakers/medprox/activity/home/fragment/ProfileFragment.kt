d74937b254351b7bb745d2df767f478d87a3c5f8
Eric Ampire <ericampire.top@gmail.com>
2018-11-15 07:15:01 +0200

    Ajout du titre dans le profile

--- app/src/main/java/com/pbreakers/medprox/activity/home/fragment/ProfileFragment.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/home/fragment/ProfileFragment.kt
@@ -12,9 +12,18 @@ import com.pbreakers.medprox.R
 
 class ProfileFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "MEDPROX | Profile"
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
