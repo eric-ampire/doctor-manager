f6947c21c784a8e11a5d25a3e38b0590250f0bdc
Eric Ampire <ericampire.top@gmail.com>
2018-11-15 16:44:46 +0200

    C'est pas safe de travailler avec des chaines en dure

--- app/src/main/java/com/pbreakers/medprox/activity/authentification/SplashActivity.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/authentification/SplashActivity.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.pbreakers.medprox.R
 import com.pbreakers.medprox.activity.home.MainActivity
+import com.pbreakers.medprox.model.User
 
 class SplashActivity : AppCompatActivity() {
 
@@ -17,13 +19,15 @@ class SplashActivity : AppCompatActivity() {
             override fun run() {
                 Thread.sleep(1000)
 
-                val userPreferences = getSharedPreferences("user-info", Context.MODE_PRIVATE)
+                val userPreferences = baseContext.getSharedPreferences(User.KEY_USER, Context.MODE_PRIVATE)
 
-                if (userPreferences == null) {
-                    startActivity(Intent(baseContext, LoginActivity::class.java))
+
+                if (userPreferences.contains(User.LOGIN_USER) and userPreferences.contains(User.PASSWORD_USER)) {
+                    startActivity(Intent(baseContext, MainActivity::class.java))
                     finish()
                 } else {
-                    startActivity(Intent(baseContext, MainActivity::class.java))
+
+                    startActivity(Intent(baseContext, LoginActivity::class.java))
                     finish()
                 }
             }
