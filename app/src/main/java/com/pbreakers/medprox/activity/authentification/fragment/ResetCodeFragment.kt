6216a634b95cb769e3a24ad25f9f1a055bbe0c10
Eric Ampire <ericampire.top@gmail.com>
2018-11-15 07:12:10 +0200

    Sauvegarde des informations durant le cycle de vie de l'application Reaction du clique quand on valide le code de reinitialisation

--- app/src/main/java/com/pbreakers/medprox/activity/authentification/fragment/ResetCodeFragment.kt
+++ app/src/main/java/com/pbreakers/medprox/activity/authentification/fragment/ResetCodeFragment.kt
@@ -6,17 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
 
 import com.pbreakers.medprox.R
+import kotlinx.android.synthetic.main.fragment_reset_code.*
 
 class ResetCodeFragment : Fragment() {
 
-    override fun onPause() {
-        super.onPause()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "MEDPROX | Réinitialisation"
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        if (edCodeVerification.text != null && edCodeVerification.text!!.isBlank()) {
+            outState.putCharSequence("code", edCodeVerification.text.toString())
+        }
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        val restoredCode = savedInstanceState?.getCharSequence("code")
+
+        if (restoredCode != null) {
+            edCodeVerification.setText(restoredCode)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +45,15 @@ class ResetCodeFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        verificationCodeButton.setOnClickListener {
+            // Si le code correspond
+            val codeVerification = edCodeVerification.text.toString()
 
+            Navigation.findNavController(it).navigate(R.id.action_resetCodeFragment_to_passwordFragment)
+        }
 
+        tvResendCode.setOnClickListener {
+            Toast.makeText(activity, "Vous aller bientot recevoir le code de reinitialisation", Toast.LENGTH_LONG).show()
+        }
     }
 }
